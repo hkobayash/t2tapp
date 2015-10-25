84a7ed922cba6a61268607504c84da7ddb7454f7
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-10-25 16:40:02 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "io"
+    "net/http"
+    "html/template"
+    "github.com/zenazn/goji"
+    "github.com/zenazn/goji/web"
+)
+
+func init() {
+    http.Handle("/", goji.DefaultMux)
+
+    goji.Get("/", indexHandler)
+}
+
+func render(v string, w io.Writer, data map[string]interface{}) {
+    tmpl := template.Must(template.ParseFiles("views/layout.html", v))
+    tmpl.Execute(w, data)
+}
+
+func indexHandler(c web.C, w http.ResponseWriter, r *http.Request) {
+    data := map[string]interface{}{
+        "Name": "home",
+    }
+    render("views/index.html", w, data)
+}
