da813b7efc4370f035769e092f12d3425e83420c
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-10-28 06:28:59 +0900

    update models

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "io"
-    "net/http"
-    "html/template"
-    "github.com/zenazn/goji"
-    "github.com/zenazn/goji/web"
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 )
 
 func init() {
-    http.Handle("/", goji.DefaultMux)
+	http.Handle("/", goji.DefaultMux)
 
-    goji.Get("/", indexHandler)
+	goji.Get("/", indexHandler)
 }
 
 func render(v string, w io.Writer, data map[string]interface{}) {
-    tmpl := template.Must(template.ParseFiles("views/layout.html", v))
-    tmpl.Execute(w, data)
+	tmpl := template.Must(template.ParseFiles("views/layout.html", v))
+	tmpl.Execute(w, data)
 }
 
 func indexHandler(c web.C, w http.ResponseWriter, r *http.Request) {
-    data := map[string]interface{}{
-        "Name": "home",
-    }
-    render("views/index.html", w, data)
+	data := map[string]interface{}{
+		"Name": "home",
+	}
+	render("views/index.html", w, data)
 }
--- models.go
+++ models.go
@@ -1,26 +1,34 @@
 package main
 
 import (
-    "time"
-    "google.golang.org/appengine"
+	"time"
+
+	"google.golang.org/appengine"
 )
 
-type Editor struct {
-    Name string `json:"name" datastore:"Name"`
-    Email string `json:"email" datastore:"Email"`
-    IconURL string `json:"icon_url" datastore:"IconURL"`
-    UpdatedAt time.Time `json:"updated_at" datastore:"UpdatedAt"`
-    CreatedAt time.Time `json:"created_at" datastore:"CreatedAt"`
+// User is the kind which stores data of users who comment and have favourite list
+type User struct {
+	Name       string    `json:"name" datastore:"Name"`
+	Email      string    `json:"email" datastore:"Email"`
+	IconURL    string    `json:"icon_url" datastore:"IconURL"`
+	Favourites []string  `json:"favourites" datastore:"Favourites"` // list of SpotCode
+	IsEditor   bool      `json:"is_editor" datastore:"IsEditor"`
+	IsAdmin    bool      `json:"is_admin" datastore:"IsAdmin"`
+	UpdatedAt  time.Time `json:"updated_at" datastore:"UpdatedAt"`
+	CreatedAt  time.Time `json:"created_at" datastore:"CreatedAt"`
 }
 
+// Spot is the kind which stores sightseeing spot information
+// Set SpotCode as KeyName
 type Spot struct {
-    SpotCode string `json:"spot_code" datastore:"SpotCode"`
-    Revision int `json:"revision" datastore:"Revision"`
-    SpotName string `json:"spot_name" datastore:"SpotName"`
-    Body string `json:"body" datastore:"Body,noindex"`
-    Photos []string `json:"photos" datastore:"Photos"`
-    GeoInfo appengine.GeoPoint `json:"geo_info" datastore:"GeoInfo"`
-    Phone string `json:"phone" datastore:"Phone"`
-    UpdatedAt time.Time `json:"updated_at" datastore:"UpdatedAt"`
-    CreatedAt time.Time `json:"created_at" datastore:"CreatedAt"`
+	SpotCode       string             `json:"spot_code" datastore:"SpotCode"`
+	RevisionNumber int                `json:"revision" datastore:"Revision"` // increment on update
+	Status         string             `json:"status" datastore:"Status"`     // 'status' or 'revision'
+	SpotName       string             `json:"spot_name" datastore:"SpotName"`
+	Body           string             `json:"body" datastore:"Body,noindex"`
+	Photos         []string           `json:"photos" datastore:"Photos"`
+	GeoInfo        appengine.GeoPoint `json:"geo_info" datastore:"GeoInfo"`
+	Phone          string             `json:"phone" datastore:"Phone"`
+	UpdatedAt      time.Time          `json:"updated_at" datastore:"UpdatedAt"`
+	CreatedAt      time.Time          `json:"created_at" datastore:"CreatedAt"`
 }
