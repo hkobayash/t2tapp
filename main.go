b39f486ac61169a7b079155e5625fd953062c4d1
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2015-11-30 06:06:28 +0900

    add route for non auth api

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func init() {
 	http.Handle("/", goji.DefaultMux)
 
 	goji.Get("/", indexHandler)
-	goji.Get("/edit/", indexHandler)
 	goji.Get("/api/v1/spots", spotHandler)
+	goji.Get("/api/v1/spots/:spotCode", spotGetHandler)
+	goji.Get("/edit/", indexHandler)
 	goji.Get("/edit/v1/spots", spotHandler)
 	goji.Get("/edit/v1/spots/:spotCode", spotGetHandler)
 	goji.Post("/edit/v1/spots", spotCreateHandler)
